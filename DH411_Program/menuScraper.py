a0a60ea15c071f6cd74367d120c9487cfa404894
ralphcmiller <46081359+ralphcmiller@users.noreply.github.com>
2022-03-03 13:37:15 -0800

    Able to print out all menu items in order with their time of day header ie Bfast, ect. ready to be put into DB structure

--- DH411_Program/menuScraper.py
+++ DH411_Program/menuScraper.py
@@ -2,17 +2,24 @@
 Created 2/24/2022
 @author: Ralph Miller
 """
+import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
+
+## Uses headless browser to grab HTML of specific DH menu ##
 driver = webdriver.Chrome()
 driver.get('https://nutrition.sa.ucsc.edu/')
-driver.find_element_by_link_text("Colleges Nine & Lewis Dining Hall").click()
+driver.find_element_by_link_text("College Nine/John R. Lewis Dining Hall").click()
 pageSource = driver.page_source
 soup = BeautifulSoup(pageSource, 'html.parser')
 driver.close()
 
-for meal in soup.find_all('div', attrs={"class":"shortmenumeals"}):
-    print(meal)
-    for item in soup.find_all('div', attrs={"class":"shortmenurecipes"}):
-        print(item.text)
-    print(" ")
+## Parses HTML with bs4 and find all menu items ##
+menuTable = soup.find('table',  {'bordercolor' : '#CCC'})   # Finds meal table
+meal = menuTable.findAll("tr")                              # Finds each seperate meal table (Bfast, Lunch, ect)
+
+for meal in menuTable:                                      # For each item in the meal table, strip empty text
+    text = meal.text.strip()                                # and save the menu item
+    meal.string = re.sub(r"[\n][\W]+[^\w]", "\n", text)
+print(meal.text)                                            # print out menu items
+
