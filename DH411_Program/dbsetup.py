bfb5b4e01070857158ac1ba9779febb825b9a898
nils <nils@ucsc.edu>
2022-03-03 18:06:50 -0800

    adding category/subcategory

--- DH411_Program/dbsetup.py
+++ DH411_Program/dbsetup.py
@@ -5,11 +5,11 @@ def CreateMenuDB():
     connection = sqlite3.connect('DHmenus.db')
     cursor = connection.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS DiningHallMenus
-                  (Date TEXT, DiningHall TEXT, MealName TEXT, Eggs INT, Fish INT, GF INT, Dairy INT, Peanuts INT, Soy INT, TreeNuts INT, Vegan INT, Vegetarian INT, Pork INT, Beef INT, Halal INT, Shellfish INT)''')
+                  (Date TEXT, DiningHall TEXT, MealName TEXT, MealCategory TEXT, MealSubcategory TEXT, Eggs INT, Fish INT, GF INT, Dairy INT, Peanuts INT, Soy INT, TreeNuts INT, Vegan INT, Vegetarian INT, Pork INT, Beef INT, Halal INT, Shellfish INT)''')
     connection.commit()
     connection.close()
 
-def AddMenuItem(Date, DiningHall, MealName, MealTime, Eggs, Fish, GF, Dairy, Peanuts, Soy, TreeNuts, Vegan, Vegetarian, Pork, Beef, Halal, Shellfish):
+def AddMenuItem(Date, DiningHall, MealName, MealCategory, MealSubcategory, Eggs, Fish, GF, Dairy, Peanuts, Soy, TreeNuts, Vegan, Vegetarian, Pork, Beef, Halal, Shellfish):
     #try to connect to database
     #if it doesnt connect create the database
     connection = None
@@ -20,7 +20,7 @@ def AddMenuItem(Date, DiningHall, MealName, MealTime, Eggs, Fish, GF, Dairy, Pea
 
     #Generate SQL Query and insert Data
     sql = f''' INSERT INTO DiningHallMenus(Date, DiningHall, MealName, MealTime, Eggs, Fish, GF, Dairy, Peanuts, Soy, TreeNuts, Vegan, Vegetarian, Pork, Beef, Halal, Shellfish)
-                  VALUES({Date},{DiningHall},{MealName},{MealTime},{Eggs},{Fish},{GF},{Dairy},{Peanuts},{Soy},{TreeNuts},{Vegan},{Vegetarian},{Pork},{Beef},{Halal},{Shellfish}) '''
+                  VALUES({Date},{DiningHall},{MealName},{MealCategory},{MealSubcategory},{Eggs},{Fish},{GF},{Dairy},{Peanuts},{Soy},{TreeNuts},{Vegan},{Vegetarian},{Pork},{Beef},{Halal},{Shellfish}) '''
     cur = connection.cursor()
     cur.execute(sql)
     connection.commit()
